2c3fd1b9cd8e31ef1f92431fd896119b8f47397a
Andrew Hyatt <ahyatt@gmail.com>
2018-01-20 16:14:16 -0500

    Add GPL v2 license, as well as project metadata.

--- org-project.el
+++ org-project.el
@@ -1,5 +1,24 @@
 ;;; org-project.el --- Link org headings with projects -*- lexical-binding:t -*-
 
+;; Copyright (c) 2018 Free Software Foundation, Inc.
+
+;; Author: Andrew Hyatt <ahyatt@gmail.com>
+;; Keywords: Org-mode, Projects, Projectile, Eshell
+;; Version: 0.1
+;; Package-Requires: ((cl-lib "0.5"))
+;;
+;; This program is free software; you can redistribute it and/or
+;; modify it under the terms of the GNU General Public License as
+;; published by the Free Software Foundation; either version 3 of the
+;; License, or (at your option) any later version.
+;;
+;; This program is distributed in the hope that it will be useful, but
+;; WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+;; General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with GNU Emacs.  If not, see <http://www.gnu.org/licenses/>.
 
 ;;; Commentary:
 ;; This package allows the user to set a project property in org headings,
