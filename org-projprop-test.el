5202be0b87fde2f017e043010386708340f7f025
Andrew Hyatt <ahyatt@gmail.com>
2018-03-25 21:25:08 -0400

    Rename org-project to org-projprop.
    To fix the naming conflict with the other org-project, rename this to
    org-projprop. This name was chosen to be relatively short and underscore this is
    all about setting and using a project property.
    
    IMPORTANT!
    This increases the version to 1.0, because it is a breaking change.
    
    To fix your config, please rename all instances of org-project to org-projprop.

--- org-projprop-test.el
+++ org-projprop-test.el
@@ -1,29 +1,29 @@
-;;; org-project-test.el --- Tests for org-project
+;;; org-projprop-test.el --- Tests for org-projprop
 
 ;;; Commentary:
-;; ert tests testing a variety of `org-project' functionality.
+;; ert tests testing a variety of `org-projprop' functionality.
 
 (require 'ert)
 (require 'cl)
 
-(ert-deftest org-project-list-directory ()
+(ert-deftest org-projprop-list-directory ()
   (cl-letf (((symbol-function 'directory-files)
              (lambda (dir &optional full match nosort) '("a" "b"))))
-    (should (equal (org-project-list-directory "~/tmp")
+    (should (equal (org-projprop-list-directory "~/tmp")
                    '(("a" . "~/tmp/a") ("b" . "~/tmp/b"))))
-    (should (equal (org-project-list-directory "~/tmp/")
+    (should (equal (org-projprop-list-directory "~/tmp/")
                    '(("a" . "~/tmp/a") ("b" . "~/tmp/b"))))))
 
-(ert-deftest org-project-list-projectile ()
+(ert-deftest org-projprop-list-projectile ()
   (cl-letf (((symbol-function 'projectile-relevant-known-projects)
              (lambda () '("~/foo/bar" "~/tmp/baz"))))
     (should (equal '(("bar" . "~/foo/bar")
                      ("baz" . "~/tmp/baz"))
-                   (org-project-list-projectile)))))
+                   (org-projprop-list-projectile)))))
 
 ;; TODO: Test more thoroughly. I had a test before, but it hung emacs, for
 ;; reasons I haven't yet figured out (probably having something to do with the
 ;; use of cl-letf).
-(ert-deftest org-project-list ()
-  (let ((org-project-list-funcs))
-    (should-error (org-project-list))))
+(ert-deftest org-projprop-list ()
+  (let ((org-projprop-list-funcs))
+    (should-error (org-projprop-list))))
--- org-projprop.el
+++ org-projprop.el
@@ -1,10 +1,10 @@
-;;; org-project.el --- Link org headings with projects -*- lexical-binding:t -*-
+;;; org-projprop.el --- Link org headings with projects -*- lexical-binding:t -*-
 
 ;; Copyright (c) 2018 Free Software Foundation, Inc.
 
 ;; Author: Andrew Hyatt <ahyatt@gmail.com>
 ;; Keywords: Org-mode, Projects, Projectile, Eshell
-;; Version: 0.1
+;; Version: 1.0
 ;; Package-Requires: ((cl-lib "0.5"))
 ;;
 ;; This program is free software; you can redistribute it and/or
@@ -24,94 +24,94 @@
 ;; This package allows the user to set a project property in org headings,
 ;; and use those properties to open a project in a variety of ways.
 ;;
-;; To use, at a minimum, configure `org-project-list-funcs' which defines
+;; To use, at a minimum, configure `org-projprop-list-funcs' which defines
 ;; what the projects available are, and in what directory they are located.
 ;;
-;; It is also necessary to call (org-project-setup) to integrate into org mode.
+;; It is also necessary to call (org-projprop-setup) to integrate into org mode.
 ;;
 ;; Once this is done, you can set a property "PROJECT" (or whatever the value of
-;; `org-project-property-name' is) and list of project names should be available
+;; `org-projprop-property-name' is) and list of project names should be available
 ;; for completion. Choosing one should, if you have the `persp-mode' package
 ;; installed, open a perspective, and put both an eshell in the project
 ;; directory, and a narrowed org-mode buffer in the perspective. Without
 ;; `persp-mode', both the eshell and org mode buffer will still be created, just
 ;; not in a perspective. The behavior of opening a project is defined with
-;; `org-project-open-funcs', and can also be configured.
+;; `org-projprop-open-funcs', and can also be configured.
 
 ;;; Code:
 
-(defgroup org-project nil
-  "Customization for the `org-project' package."
-  :prefix 'org-project
+(defgroup org-projprop nil
+  "Customization for the `org-projprop' package."
+  :prefix 'org-projprop
   :version "25.2.1"
-  :package-version '(org-project . "0.1")
+  :package-version '(org-projprop . "0.1")
   :group 'org)
 
-(defcustom org-project-property-name "PROJECT"
+(defcustom org-projprop-property-name "PROJECT"
   "The name for the org property to store the project."
-  :group 'org-project
+  :group 'org-projprop
   :type '(string))
 
 ;; This can't be customized easily, best to leave it to elisp to setup.
-(defvar org-project-list-funcs nil
+(defvar org-projprop-list-funcs nil
   "List of functions providing project locations.
 For example:
-  (setq org-project-list-funcs
+  (setq org-projprop-list-funcs
      '((directory \"~/src/\")
        (directory \"~/Dropbox/src/\")
        (projectile)))
 
 Each list item is evaluated by taking the first symbol and
-calling a function prepended by org-project-list-. You can write
-your own function called, for example, org-project-list-foo, and
+calling a function prepended by org-projprop-list-. You can write
+your own function called, for example, org-projprop-list-foo, and
 refer to it via the symbol `foo' here.  The other parts of each
 entry are the arguments that will be passed to the function.
 
 The `directory' and `projectile' methods are already available to
 use.")
 
-(defcustom org-project-eshell-format "*eshell-%s*"
+(defcustom org-projprop-eshell-format "*eshell-%s*"
   "Format string for eshell buffers."
-  :group 'org-project
+  :group 'org-projprop
   :type '(string))
 
-(defcustom org-project-org-buffer-format "*org-%s*"
+(defcustom org-projprop-org-buffer-format "*org-%s*"
   "Format string for org buffers."
-  :group 'org-project
+  :group 'org-projprop
   :type '(string))
 
-(defcustom org-project-open-funcs '(persp eshell org-buffer)
+(defcustom org-projprop-open-funcs '(persp eshell org-buffer)
   "List of functions to call when opening a project.
 Each is called in order and each calls a function prepended by
-`org-project-open-'. Each function takes as an argument the
+`org-projprop-open-'. Each function takes as an argument the
 project name and the project directory. Each is called without
 save the excursion, so any windowing changes are seen by the
 user. You can define your own functions following that pattern
 and reference them here."
-  :group 'org-project
+  :group 'org-projprop
   :type '(repeat symbol))
 
-(defun org-project-list-directory (dir)
+(defun org-projprop-list-directory (dir)
   "Projects are subdirectories of DIR."
   (mapcar
    (lambda (d)
      (cons d (concat (directory-file-name dir) "/" d)))
    (directory-files dir nil "^[^.]")))
 
-(defun org-project-list-projectile ()
+(defun org-projprop-list-projectile ()
   "Projects are projectile known projects."
   (mapcar (lambda (p) (cons (file-name-nondirectory p) p))
           (projectile-relevant-known-projects)))
 
-(defun org-project-open-eshell (name dir)
+(defun org-projprop-open-eshell (name dir)
   "Open an eshell for project called NAME in directory DIR."
   (require 'eshell)
   (let ((default-directory dir))
     (cl-letf (((symbol-value 'eshell-buffer-name)
-               (format org-project-eshell-format name)))
+               (format org-projprop-eshell-format name)))
       (eshell))))
 
-(defun org-project-open-persp (name dir)
+(defun org-projprop-open-persp (name dir)
   "Open a perspective for the project called NAME.
 Add all buffers for files under DIR to the perspective. If the
 persp package is not installed, do nothing. This only makes sense
@@ -130,16 +130,16 @@ non-nil."
     ;; Now that we have a new perspective, clear up the windows.
     (delete-other-windows)))
 
-(defun org-project-open-org-buffer (name dir)
+(defun org-projprop-open-org-buffer (name dir)
   "Create a new indirect buffer with only the relevant part of the org file.
 Specifically, it's the same org file but with an indirect
 buffer, narrowed to the parent heading with the project NAME."
-  (let* ((buf-name (format org-project-org-buffer-format name))
+  (let* ((buf-name (format org-projprop-org-buffer-format name))
          (buf (or (get-buffer buf-name)
                   (make-indirect-buffer (current-buffer) buf-name t))))
     (switch-to-buffer buf)
     (let ((orig-point (point)))
-      (while (not (org-entry-get (point) org-project-property-name))
+      (while (not (org-entry-get (point) org-projprop-property-name))
         (outline-up-heading 1 t))
       (org-narrow-to-subtree)
       (goto-char orig-point))
@@ -150,40 +150,40 @@ buffer, narrowed to the parent heading with the project NAME."
       ;; manually.
       (persp-after-change-major-mode-h))))
 
-(defun org-project-open ()
+(defun org-projprop-open ()
   "Open the project at the current org entry."
   (interactive)
   (let* ((buf (current-buffer))
          (name
-          (org-entry-get nil org-project-property-name t))
+          (org-entry-get nil org-projprop-property-name t))
          (dir
-          (cdr (assoc name (org-project-list))))
+          (cdr (assoc name (org-projprop-list))))
          (point (point)))
-    (dolist (f org-project-open-funcs)
+    (dolist (f org-projprop-open-funcs)
       ;; Each function should assume it starts at the same place.
       (with-current-buffer buf
         (goto-char point)
-        (funcall (intern (format "org-project-open-%s" (symbol-name f))) name dir)))))
+        (funcall (intern (format "org-projprop-open-%s" (symbol-name f))) name dir)))))
 
-(defun org-project-property-values (prop)
+(defun org-projprop-property-values (prop)
   "Function to populate valid property values for org property setting.
 PROP is the property being set, and this function should ignore
 all properties except for the project property."
-  (when (equal prop org-project-property-name)
+  (when (equal prop org-projprop-property-name)
     (append (mapcar #'car
-             (org-project-list)))))
+             (org-projprop-list)))))
 
-(defun org-project-setup ()
-  "Setup function to enable org-project behavior."
+(defun org-projprop-setup ()
+  "Setup function to enable org-projprop behavior."
   (with-eval-after-load 'org
     (add-to-list 'org-property-allowed-value-functions
-                 'org-project-property-values)))
+                 'org-projprop-property-values)))
 
-(defun org-project-list ()
+(defun org-projprop-list ()
   "Return a list of projects available.
 The strategy for getting that list is based on
-`org-project-list-func'."
-  (if org-project-list-funcs
+`org-projprop-list-func'."
+  (if org-projprop-list-funcs
       (cl-remove-if
        'null
        (cl-remove-duplicates
@@ -191,12 +191,12 @@ The strategy for getting that list is based on
          (lambda (e)
            (apply
             (intern
-             (concat "org-project-list-" (symbol-name (car e))))
+             (concat "org-projprop-list-" (symbol-name (car e))))
             (cdr e)))
-         org-project-list-funcs)
+         org-projprop-list-funcs)
         :test 'equal))
-    (error "org-project-list-func not defined, cannot get projects.")))
+    (error "org-projprop-list-func not defined, cannot get projects.")))
 
-(provide 'org-project)
+(provide 'org-projprop)
 
-;;; org-project.el ends here
+;;; org-projprop.el ends here
